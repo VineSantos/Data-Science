ebaef7e632256801fb1c37fe4736c9e26a25814a
Vinicius da Silva Santos <vine.santos28@gmail.com>
2025-09-23 18:22:03 -0300

    exercicio 9 finalizado e inclusão do 10

--- datascience-9.py
+++ datascience-9.py
@@ -4,6 +4,15 @@ ano22 = int(input("Digite quantas vendas teve no ano de 2022: "))
 
 ano23 = int(input("Digite quantas vendas teve no ano de 2023: "))
 
-variacao_porcentual = ((ano22 - ano23) / ano22) * 100
+variacao_porcentual = ((ano23 - ano22) / ano22) * 100
+variacao_formatada = round(variacao_porcentual, 2)
 
-print(f"{variacao_porcentual}")
+
+if variacao_porcentual > 20:
+    print(f"Aumento de {variacao_porcentual}%!! Bonificação para toda equipe!")
+elif 2 <= variacao_porcentual <= 20:
+    print(f"Aumento de {variacao_porcentual}%!! Bonificação parcial para toda equipe")
+elif -10 <= variacao_porcentual <= 2:
+    print(f"Aumento de {variacao_porcentual}%!! planejamento de políticas de incentivo às vendas")
+elif variacao_porcentual < -10:
+    print(f"Variação de {variacao_porcentual}%!! Corte de gastos")
