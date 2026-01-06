8e120a3c7d63b985c33d473e7eec598aca2d495d
Vinicius da Silva Santos <vine.santos28@gmail.com>
2026-01-06 18:34:50 -0300

    vamo que vamo

---
+++ estrutura_de_dados/estrutura_2.py
@@ -0,0 +1,16 @@
+ocorrencias = [
+    "Roubo", "Furto", "Roubo", "Agressão",
+    "Furto", "Furto", "Roubo", "Agressão"
+]
+
+Tarefas:
+
+Crie um dicionário que conte quantas vezes cada ocorrência aparece.
+
+Descubra qual ocorrência é mais frequente.
+
+Exiba o resultado no formato:
+
+Roubo: 3
+Furto: 3
+Agressão: 2
