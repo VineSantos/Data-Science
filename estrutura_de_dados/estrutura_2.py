103a19758b9844822af2caa4dcad7d49d7f4f702
Vinicius da Silva Santos <vine.santos28@gmail.com>
2026-01-09 18:15:38 -0300

    novo exercicio feito

--- estrutura_de_dados/estrutura_2.py
+++ estrutura_de_dados/estrutura_2.py
@@ -3,14 +3,17 @@ ocorrencias = [
     "Furto", "Furto", "Roubo", "Agressão"
 ]
 
-Tarefas:
+contagem = {}
 
-Crie um dicionário que conte quantas vezes cada ocorrência aparece.
+for item in ocorrencias:
+    if item in contagem:
+        contagem[item] += 1
+    else:
+        contagem[item] = 1
+        
+for ocorrencias, quantidade in contagem.items():
+    print(f"{ocorrencias}: {quantidade}")
+    
+mais_frequente = max(contagem, key=contagem.get)
 
-Descubra qual ocorrência é mais frequente.
-
-Exiba o resultado no formato:
-
-Roubo: 3
-Furto: 3
-Agressão: 2
+print(f"Ocorrencis mais frenquente: {mais_frequente}({contagem[mais_frequente]})")
