0f72e9751763fcb5b81c7c43842e8f29b5cc170c
Vinicius da Silva Santos <vine.santos28@gmail.com>
2026-01-20 18:50:35 -0300

    exercicio 3

---
+++ estrutura_de_dados/estrutura_3.py
@@ -0,0 +1,6 @@
+pessoas = [
+    {"nome": "Ana", "idade": 23, "salario": 3200},
+    {"nome": "Carlos", "idade": 30, "salario": 4500},
+    {"nome": "Bianca", "idade": 27, "salario": 3800},
+    {"nome": "Daniel", "idade": 35, "salario": 5200}
+]
