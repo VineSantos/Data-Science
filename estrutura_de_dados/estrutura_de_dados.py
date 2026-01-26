8007ba51224e03b572dfef7c7ebdcdc496a55388
Vinicius da Silva Santos <vine.santos28@gmail.com>
2026-01-26 18:18:09 -0300

    aee

--- estrutura_de_dados/estrutura_de_dados.py
+++ estrutura_de_dados/estrutura_de_dados.py
@@ -4,7 +4,5 @@
 
 gastos_anual = [2172.54, 3701.35, 3518.09, 3456.61, 3249.38, 2840.82, 3891.45, 3075.26, 2317.64, 3219.08]
 
-media = sum(gastos_anual) / len(gastos_anual)
 
-print(f"Média de gastos: R$ {media:.2f}")
 
